1e0e32babdb6bcc36daf6c2242d98499773d8d84
Dinhlamnolu <88892350+Dinhlamnolu@users.noreply.github.com>
2021-08-13 20:50:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 const CONFIG = {
     introTitle: 'Helu Ánh Sao của anh!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Đang buồn đang chán thì 
+    có 1 cô bé đáng iu và dễ thương 
+    nhảy vào cuộc đời anhhh :3`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
@@ -11,7 +10,7 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :D',
     question:'Trên thế giới hơn 7 tỉ người mà sao Ánh Sao lại yêu anh hihi <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu Đình Lâm nhất trên đời lun :33',
     mess: 'Anh biết mà. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Hết dịch cho anh làm công chúa của em nheeee :3.',
     btnAccept: 'Okiiiii lun <3',
