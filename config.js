82fc73e0684c937fc5e70eeb100e0e25bcc1cdc3
Dinhlamnolu <88892350+Dinhlamnolu@users.noreply.github.com>
2021-08-18 19:52:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Helu Ánh Sao của anh!',
+    introTitle: 'Helu Thúy Vân của anh!',
     introDesc: `Đang buồn đang chán thì 
     có 1 cô bé đáng iu và dễ thương 
     nhảy vào cuộc đời anhhh :3`,
@@ -8,11 +8,11 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích Đình Lâm <33',
     btnNo: 'Không, Anh trai à :D',
-    question:'Trên thế giới hơn 7 tỉ người mà sao Ánh Sao lại yêu anh hihi <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Thúy Vân lại yêu anh hihi <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu Đình Lâm nhất trên đời lun :33',
-    mess: 'Anh biết mà. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Hết dịch cho anh làm công chúa của em nheeee :3.',
+    mess: 'Anh biết mà. Yêu Vân nhiều nhiều 😘😘',
+    messDesc: 'Cho phép anh làm công chúa của Vân nheeee :3.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://facebook.com/lamdinh01' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
